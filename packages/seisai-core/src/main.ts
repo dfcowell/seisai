b23abbc14d83e368ea28c7cbcc3b581239475a2e
Dan Cowell <admin@dfcowell.net>
2019-12-07 17:55:46 +0900

    add persistent sessions with redis

--- packages/seisai-core/src/main.ts
+++ packages/seisai-core/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import session from 'express-session';
-import cookieParser from 'cookie-parser';
 import passport from 'passport';
+import createRedisStore from 'connect-redis';
+import { createClient } from 'redis';
+
+const RedisStore = createRedisStore(session);
+const redisClient = createClient({
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT ? Number(process.env.REDIS_PORT) : null,
+  password: process.env.REDIS_PASSWORD,
+  db: process.env.REDIS_DB ? Number(process.env.REDIS_DB) : 0
+});
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,12 +21,16 @@ async function bootstrap() {
       secret: process.env.SESSION_SECRET || 'secret',
       resave: false,
       saveUninitialized: true,
+      cookie: { secure: false },
+      store: new RedisStore({
+        client: redisClient,
+        ttl: 86400
+      })
     }),
   );
-  app.use(cookieParser());
   app.use(passport.initialize());
   app.use(passport.session());
 
-  await app.listen(3001);
+  await app.listen(process.env.PORT ? Number(process.env.PORT) : 3000);
 }
 bootstrap();
